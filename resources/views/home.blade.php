2802864c9862080c97dec225c094a2ed467aa7d1
Gklam <khalid.lamsadi@outlook.fr>
2020-03-09 17:43:56 +0100

    00:43:31 complete profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,20 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row ">
+        <div class="col-3 p-5">
+            <img src="img/img1.jpg" class="rounded-circle">
+        </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="col-9 pt-5">
+            <div><h1>FreeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>22,523</strong> posts</div>
+                <div class="pr-5"><strong>51,7m</strong> followers</div>
+                <div class="pr-5"><strong>30</strong> following</div>
+            </div> 
+            <div class="pt-4 font-weight-bold ">9GAG: Go Fun The World</div>
+            <div>
+                🌟 Get our app for the latest and funniest MEMES and VIDEOS 👉🏻@9gagmobile
+                <br>
+                -
+                <br>
+                Get your hamster tissue box cover 🐹 by @takemymoney👇🏻
+            </div>
+            <div class="font-weight-bold"><a href="https://bit.ly/hamsterbox">https://bit.ly/hamsterbox</a> </div>
+        
+        </div>
+        
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=1" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=2" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=3" class="w-100">
+            </div>
+        </div>
 
-                    You are logged in!
-                </div>
+        <div class="row pt-5">
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=4" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=5" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="https://picsum.photos/320/290?random=6" class="w-100">
             </div>
         </div>
     </div>
